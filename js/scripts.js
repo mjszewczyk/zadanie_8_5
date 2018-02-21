324b7e2fb78cfe0a9f172822b0c0a45f973b3d06
Magdalena Szewczyk <mjszewczyk91@gmail.com>
2018-02-21 21:46:48 +0100

    Add index.html and scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+// obliczanie pola trójkąta
+var a = prompt('Podaj długość podstawy');
+var h = prompt('Podaj wysokość');
+var triangleArea = a * h / 2;
+alert('Pole trójkąta to ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
